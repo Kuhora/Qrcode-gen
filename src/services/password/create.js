076f426c78e5f889e755729754e51be967af5cfe
kuhora <kuhora123@gmail.com>
2024-10-21 18:15:49 -0300

    Finished

--- src/services/password/create.js
+++ src/services/password/create.js
@@ -1,10 +1,11 @@
-import chalk from "chalk"
-import handle from "./handle";
+import chalk from "chalk";
+import handle from './utils/handle.js';
 
 
-async function createPassword(){
+
+async function createPassword() {
     console.log(chalk.green("password"));
-    const password = await handle()
+    const password = await handle();
     console.log(password);
 }
 
